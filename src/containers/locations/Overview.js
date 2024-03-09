fc7999c11647a9ffd393c4b243b4875ca4d29f6f
andrewcook <andrewjcook93@gmail.com>
2024-03-08 22:43:24 -0600

    Feature: Add directions buttons to location overview

--- src/containers/locations/Overview.js
+++ src/containers/locations/Overview.js
@@ -31,10 +31,16 @@ function Overview() {
                     </span>
                 </h1>
                 {/* Buttons */}
-                <a className='location-overview__header--button' href='drewford.dev' target='_blank'>
+                <a className='location-overview__header--button' 
+                    href={location.pathname === '/locations/livingston' ? 
+                        'https://drewford.dev' 
+                        :'https://drewford.dev'} target='_blank'>
                         Schedule Appointment
                 </a>
-                <a className='location-overview__header--button' href='drewford.dev' target='_blank'>
+                <a className='location-overview__header--button' 
+                    href={location.pathname === '/locations/livingston' ? 
+                        'https://www.google.com/maps/place/Walmart+Supercenter/@30.7159202,-94.9575004,17z/data=!3m1!4b1!4m6!3m5!1s0x86388698b08d0d17:0x8cc402e4d5031f7e!8m2!3d30.7159156!4d-94.9549255!16s%2Fg%2F11bxbb1nls?entry=ttu' 
+                        :'https://www.google.com/maps/place/Target+Optical/@30.1332134,-95.2356684,17z/data=!3m1!4b1!4m6!3m5!1s0x86474d9a671ddbfd:0xba677e390abf74d6!8m2!3d30.1332134!4d-95.2330935!16s%2Fg%2F11l2v2d8qz?entry=ttu'} target='_blank'>
                     Directions
                 </a>    
             </div>
