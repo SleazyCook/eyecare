be077f20c34103ff1b1bf8ddf0d669c9ae548b4b
andrewcook <andrewjcook93@gmail.com>
2024-03-08 22:35:57 -0600

    Style: start location overview styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 import './styles/base.css'
 import './styles/layout.css'
+import './styles/locations.css'
 
 import Header from './layout/Header';
 import Toolbar from './layout/toolbar/Toolbar';
--- src/containers/locations/Overview.js
+++ src/containers/locations/Overview.js
@@ -4,14 +4,41 @@ function Overview() {
     const location = useLocation()
 
     return(
-        <div>
-            Overview of &nbsp;
-            {location.pathname === '/locations/livingston' ?
-            
-            <>Livingston Location</>
-            :
-            <>New Caney Location</>
-            }
+        <div className={location.pathname === '/locations/livingston' ? 
+            'location-overview location-overview__livingston' : 
+            'location-overview location-overview__new-caney'}>
+
+            {/* Text Area */}
+            <div className='location-overview__header'>
+                {/* Header Text */}
+                <h1>
+                    <span className='location-overview__header--primary'>
+                        {location.pathname === '/locations/livingston' ?
+                            <>Livingston Office</>
+                            :
+                            <>New Caney Office</>
+                        }
+                    </span>
+                    <span className={location.pathname === '/locations/livingston' ? 
+                        'location-overview__header--secondary location-overview__header--secondary-livingston' : 
+                        'location-overview__header--secondary location-overview__header--secondary-new-caney'}>
+                        {/* Partnered with &nbsp; */}
+                        {location.pathname === '/locations/livingston' ?
+                            <img src='https://i.imgur.com/3b6WWwS.png' />
+                            :
+                            <img src='https://i.imgur.com/SrV68sw.png' />
+                        }
+                    </span>
+                </h1>
+                {/* Buttons */}
+                <a className='location-overview__header--button' href='drewford.dev' target='_blank'>
+                        Schedule Appointment
+                </a>
+                <a className='location-overview__header--button' href='drewford.dev' target='_blank'>
+                    Directions
+                </a>    
+            </div>
+
         </div>
     )
 }
