3d56fe5c244fb9a7876be3c80cc3e32d6eea2f1a
Andrew Cook <andrewcook@Admins-MacBook-Pro.local>
2024-03-12 11:49:32 -0500

    Much better location overview style, background image for location contact form

--- src/containers/locations/Overview.js
+++ src/containers/locations/Overview.js
@@ -27,7 +27,7 @@ function Overview() {
                                 <>New Caney</>
                             }
                         </span>
-                        <span>&nbsp; x &nbsp;</span>
+                        <span className='location-overview__header--x'>&nbsp; x &nbsp;</span>
                         <span className={location.pathname === '/locations/livingston' ? 
                             'location-overview__header--secondary location-overview__header--secondary-livingston' : 
                             'location-overview__header--secondary location-overview__header--secondary-new-caney'}>
@@ -39,6 +39,7 @@ function Overview() {
                             <br />
                         </span>
                     </h1>
+
                     {/* Phone, Fax */}
                     <div className='location-overview__flexbox'>
                         <div className='location-overview__icon-number'>
@@ -55,26 +56,22 @@ function Overview() {
                                 :
                                 <>(832) 432-6170</>}
                         </div>
-                    </div>
-
-
-
-                    
+                    </div>  
             </div>
 
             {/* Buttons */}
-            <div className='location-overview__button-box'>
-                <a className='location-overview__header--button' 
+            <div className='button-box__location-overview'>
+                <a className='button__location-overview' 
                     href={location.pathname === '/locations/livingston' ? 
                         'https://drewford.dev' 
                         :'https://drewford.dev'} target='_blank'>
                         Schedule Appointment
                 </a>
-                <a className='location-overview__header--button' 
+                <a className='button__location-overview' 
                     href={location.pathname === '/locations/livingston' ? 
                         'https://www.google.com/maps/place/Walmart+Supercenter/@30.7159202,-94.9575004,17z/data=!3m1!4b1!4m6!3m5!1s0x86388698b08d0d17:0x8cc402e4d5031f7e!8m2!3d30.7159156!4d-94.9549255!16s%2Fg%2F11bxbb1nls?entry=ttu' 
                         :'https://www.google.com/maps/place/Target+Optical/@30.1332134,-95.2356684,17z/data=!3m1!4b1!4m6!3m5!1s0x86474d9a671ddbfd:0xba677e390abf74d6!8m2!3d30.1332134!4d-95.2330935!16s%2Fg%2F11l2v2d8qz?entry=ttu'} target='_blank'>
-                    Directions
+                    Get Directions
                 </a>   
             </div> 
             
