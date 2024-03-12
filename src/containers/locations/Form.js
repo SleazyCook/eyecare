ae923ac0f1bb5060b75c84ba6b8cf5ef5d51f543
Andrew Cook <andrewcook@Admins-MacBook-Pro.local>
2024-03-12 10:20:39 -0500

    Feature: contact form added

--- src/containers/locations/Form.js
+++ src/containers/locations/Form.js
@@ -4,15 +4,26 @@ function Form() {
     const location = useLocation()
 
     return(
-        <div className='form'>
-
+        <div className='form-container'>
+{/* 
             Reach out to &nbsp;
             {location.pathname === '/locations/livingston' ?
             
             <>Livingston Location</>
             :
             <>New Caney Location</>
-            }
+            } */}
+
+            <h2>Contact Us</h2>
+            <form className='form' action="https://formsubmit.co/andrewjcook93@gmail.com" method="post">
+                <input placeholder='Your Name' type="text" id="name" name="name" required />
+
+                <input placeholder='Your Email' type="email" id="email" name="email" required />
+
+                <textarea placeholder='Message' id="message" name="message" required></textarea>
+
+                <input type="submit" value="Submit" />
+            </form>
 
         </div>
     )
