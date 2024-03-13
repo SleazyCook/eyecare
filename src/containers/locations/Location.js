8aedd9216c2dbafb20851f02e44c84085de8c399
Andrew Cook <andrewcook@Admins-MacBook-Pro.local>
2024-03-13 18:48:57 -0500

    Style: review previes buttons on home page styled

--- src/containers/locations/Location.js
+++ src/containers/locations/Location.js
@@ -1,9 +1,16 @@
+import { useEffect } from 'react'
+
 import Overview from "./Overview";
 import Office from "./Office";
 import Store from "./Store";
 import Form from "./Form";
 
 function Location() {
+
+    useEffect(() => {
+        window.scrollTo({top: 0, left: 0})
+      }, [])
+
     return(
         <div>
             <Overview />
